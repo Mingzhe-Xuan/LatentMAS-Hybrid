81f26089ca740e874143efb0b0696c49e9e08c45
Your Name <3685758901@qq.com>
2026-07-15 15:36:24 +0800

    build env

---
+++ test_libs.py
@@ -0,0 +1,5 @@
+import numpy, transformers, tokenizers, vllm
+print('numpy:', numpy.__version__)
+print('transformers:', transformers.__version__)
+print('tokenizers:', tokenizers.__version__)
+print('vllm:', vllm.__version__)
