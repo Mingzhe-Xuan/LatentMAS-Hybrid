c8337940961ac6e26555b0ba69600e64eb9f5126
Mingzhe-Xuan <xuanmingzhe@mail.ustc.edu.cn>
2026-07-22 15:00:50 +0800

    use hf instead of huggingface-cli, which has been deperacated

--- download.sh
+++ download.sh
@@ -2,5 +2,5 @@ module load python/3.12.13
 source /home/n2501945g/LatentMAS-Hybrid/.venv/bin/activate
 
 export HF_HOME=/home/n2501945g/.cache/huggingface
-huggingface-cli download Qwen/Qwen3-8B
+hf download Qwen/Qwen3-8B
 python -c "from datasets import load_dataset; load_dataset('yentinglin/aime_2025', split='train')"
